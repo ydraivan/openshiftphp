74c4f8e1524d3e5aa83b6af3c5f1af250908a04a
jyoti-gautam <68596502+jyoti-gautam@users.noreply.github.com>
2022-09-30 12:45:02 +0530

    commit1213

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP 2021  Test</h1>
-		<p><b> REDHAT OPENSHIFT DO180 </b></p>
+	<h1>PHP 2022 TESTING PAGE</h1>
+		<p><b> CITIBANK OPENSHIFT DO180 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
