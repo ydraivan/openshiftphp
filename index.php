2ff6d44cb5e67e51a96f9db03941b9353fa6c221
jyoti-gautam <68596502+jyoti-gautam@users.noreply.github.com>
2024-04-27 12:33:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	</head>
 
 <body> 
-	<h1>PHP 2022 TESTING PAGE</h1>
+	<h1>PHP 2024 TESTING PAGE</h1>
 		<p><b> CITIBANK OPENSHIFT DO180 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
