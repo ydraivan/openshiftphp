c660a8082fbfacfcb5e0eb54cd7fc25200192d65
ydraivan <161004471+ydraivan@users.noreply.github.com>
2024-04-30 14:40:27 +0545

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2028 TESTING PAGE</h1>
-		<p><b> CITIBANK OPENSHIFT DO188 </b></p>
+		<p><b> TONPL OPENSHIFT LAB DO188 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION4 </h2> 
